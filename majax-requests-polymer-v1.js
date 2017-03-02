e2016c47946d54dd10542f81b61cbf8831176964
Olufemi Adeyemi <olufemi.segun.adeyemi@desy.de>
2017-03-02 18:32:24 +0100

    readjust to proper traditional function-based class

--- majax-requests-polymer-v1.js
+++ majax-requests-polymer-v1.js
@@ -4,63 +4,57 @@
  * for polymer version 1.0.0
  * 
  * @class MultipleAjaxRequests
+ * 
+ * @constructor
+ * @param {array} urls
+ * @param {string} credential
+ * @param {!node} node
  */
-class MultipleAjaxRequests
+let MultipleAjaxRequests = function (urls, credential, node)
 {
-    constructor (urls, credential, node)
-    {
-        this._urls = urls;
-        this._credential = credential;
-        this._responseObject = [];
-        this.isResolved = false;
-        this.allResolvedEvent = new CustomEvent('allResolved', {
-            'detail': this._responseObject
-        });
-        this.node = node;
-    }
+    this._urls = urls;
+    this._credential = credential;
+    this._responseObject = [];
+    this.allResolvedEvent = new CustomEvent('allResolved', {
+        'detail': this._responseObject
+    });
+    this._node = node;
+};
+MultipleAjaxRequests.prototype.send = function()
+{
+    const textPromises = this._urls.map(url => {
+        return this._singleAjaxCall(url).then(response => response);
+    });
 
-    send()
-    {
-        const textPromises = this._urls.map(url => {
-            return this._singleAjaxCall(url).then(response => response);
-        });
-
-        textPromises.reduce((chain, textPromise) => {
-            return chain.then(() => textPromise)
+    textPromises.reduce((chain, textPromise) => {
+        return chain.then(() => textPromise)
             .then(text => {
                 this._responseObject.push(text);
                 if (this._responseObject.length == this._urls.length)
-                    this.node.dispatchEvent(this.allResolvedEvent);
+                    this._node.dispatchEvent(this.allResolvedEvent);
             });
-        }, Promise.resolve());
-    }
+    }, Promise.resolve());
+};
+MultipleAjaxRequests.prototype._singleAjaxCall = function(url)
+{
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
 
-    _singleAjaxCall (url)
-    {
-        return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest();
-            xhr.open('GET', url);
+        xhr.onload = function() {
+            if (xhr.status >= 200 || xhr.status < 500) {
+                resolve(JSON.parse(xhr.responseText));
+            } else {
+                reject(Error("Server Side Error"));
+            }
+        };
+        xhr.onerror = function() {
+            reject(Error("Network Error"));
+        };
 
-            xhr.onload = function() {
-                if (xhr.status >= 200 || xhr.status < 500) {
-                    resolve(JSON.parse(xhr.responseText));
-                } else {
-                    reject(Error("Server Side Error"));
-                }
-            };
-            xhr.onerror = function() {
-                reject(Error("Network Error"));
-            };
-
-            xhr.setRequestHeader("Content-type", "application/json");
-            xhr.setRequestHeader("Authorization", this._credential);
-            xhr.setRequestHeader("Accept", "application/json");
-            xhr.send();
-        });
-    }
-
-    get response()
-    {
-        return this._responseObject;
-    }
-}
+        xhr.setRequestHeader("Content-type", "application/json");
+        xhr.setRequestHeader("Authorization", this._credential);
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.send();
+    });
+};
