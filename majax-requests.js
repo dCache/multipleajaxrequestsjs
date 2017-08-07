ffb238c2bf4f6ec97e76761af87162de6ecc68c9
Olufemi Adeyemi <olufemi.segun.adeyemi@desy.de>
2017-08-07 23:12:57 +0200

    use eslint and make fixes

--- majax-requests.js
+++ majax-requests.js
@@ -1,5 +1,6 @@
-//quick and dirty solution. errors are not properly handled.
-'use strict';
+// Quick and dirty solution. errors are not properly handled.
+'use-strict';
+
 /**
  * Make a multiple ajax request and get a single response back
  * if all requests are completely sucessful.
@@ -8,55 +9,75 @@
  * @param {Array} urls
  * @param {?node} node. Node that will be listing to the `allResolved` event 
  * @param {Object} headers, e.g. {"Authorization": "Basic XXX"}
+ * @param {Boolean} bubbles, if true, events will bubble to ancestors of the 
+ *         element which fired the event. Default to true.
+ * @param {Boolean} cancelable, : if true, events can be canceled using 
+ *        the event object’s stopPropagation() method. Default to true.
  */
 class MultipleAjaxRequests
 {
-    constructor (urls, node, headers)
+
+    /**
+     * Creates an instance of MultipleAjaxRequests.
+     * @param {any} urls 
+     * @param {any} node 
+     * @param {any} headers 
+     * @param {any} bubbles 
+     * @param {any} cancelable 
+     * @memberof MultipleAjaxRequests
+     */
+    constructor(urls, node, headers, bubbles, cancelable)
     {
         this._urls = urls;
         this._responseObject = [];
         this.isResolved = false;
+
         /**
          * @event allResolved
          * This will be fire when all request have been processed
          */
-        this.allResolvedEvent = new CustomEvent("allResolved", {
-            "detail": this._responseObject
+        this.allResolvedEvent = new CustomEvent('allResolved', {
+            bubbles: this._getBoolean(bubbles),
+            cancelable: this._getBoolean(cancelable),
+            detail: this._responseObject,
         });
 
         this.node = node;
         this._headers = headers;
     }
+
     /**
      * 
      * @memberOf MultipleAjaxRequests
      */
     async send()
     {
-        const textPromises = this._urls.map(async url => {
-            return await this._singleAjaxCall(url);
-        });
-
-        for (const textPromise of textPromises) {
-            this._responseObject.push(await textPromise);
+        const promiseResponses = this._urls.map(
+            async (url) => {
+                const x = await this._singleAjaxCall(url);
+                return x;
+            }
+        );
+        for (const r of promiseResponses) {
+            this._responseObject.push(await r);
         }
-        if (this._responseObject.length == this._urls.length)
+        if (this._responseObject.length === this._urls.length){
             this.node.dispatchEvent(this.allResolvedEvent);
+        }
     }
 
     /**
      * 
      * @private
      * @param {String} url 
-     * @returns {Object} Promise
-     * 
+     * @return {Object} Promise
      * @memberOf MultipleAjaxRequests
      */
-    _singleAjaxCall (url)
+    _singleAjaxCall(url)
     {
-        return new Promise( (resolve, reject) => {
-            let xhr = new XMLHttpRequest();
-            xhr.open("GET", url);
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open('GET', url);
 
             xhr.onload = function() {
                 if (xhr.status >= 200 || xhr.status < 500) {
@@ -66,12 +87,13 @@ class MultipleAjaxRequests
                 }
             };
             xhr.onerror = function() {
-                reject(Error("Network Error"));
+                reject(Error('Network Error'));
             };
 
-            let key;
-            for (key in this._headers) {
-                xhr.setRequestHeader(key, this._headers[key]);
+            for (const key in this._headers) {
+                if (Object.prototype.hasOwnProperty.call(this._headers.has, key)) {
+                    xhr.setRequestHeader(key, this._headers[key]);
+                }
             }
             xhr.send();
         });
@@ -80,11 +102,22 @@ class MultipleAjaxRequests
     /**
      * 
      * @readonly
-     * 
      * @memberOf MultipleAjaxRequests
      */
     get response()
     {
         return this._responseObject;
+    }
+
+    /**
+     * 
+     * 
+     * @param {Boolan} value
+     * @return {Boolean}
+     * @memberof MultipleAjaxRequests
+     */
+    static _getBoolean(value)
+    {
+        return typeof foo === "undefined" ? true: value;
     }
 }
